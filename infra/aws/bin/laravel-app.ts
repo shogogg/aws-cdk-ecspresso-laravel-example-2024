fb9f5555c9cd3abc726f9bdb75596f970279f60d
shogogg <shogo@studiofly.net>
2024-11-06 11:42:26 +0900

    chore: Use environment variable as stack id

--- infra/aws/bin/laravel-app.ts
+++ infra/aws/bin/laravel-app.ts
@@ -9,13 +9,13 @@ dotenv.config()
 
 const account = process.env.APP_AWS_ACCOUNT ?? process.env.CDK_DEFAULT_ACCOUNT
 const region = process.env.APP_AWS_REGION ?? process.env.CDK_DEFAULT_REGION
-const appName = process.env.APP_NAME ?? 'ExampleApp'
+const stackName = process.env.APP_STACK_NAME ?? 'ExampleLaravelAppStack'
 const hostedZoneName = process.env.APP_HOSTED_ZONE_NAME ?? 'example.com'
 const domainName = process.env.APP_DOMAIN_NAME ?? 'app.example.com'
 const logBucketName = process.env.APP_LOG_BUCKET_NAME ?? 'example-log-bucket'
 
 const app = new cdk.App()
-new LaravelAppStack(app, `${appName}LaravelAppStack`, {
+new LaravelAppStack(app, stackName, {
   env: {
     account,
     region,
